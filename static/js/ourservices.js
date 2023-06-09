e93c6fa7c2f422ef0b71ba569e8ce0b5d353756a
srvinith <r36041595@gmail.com>
2023-06-09 11:55:26 +0530

    Dg

--- static/js/ourservices.js
+++ static/js/ourservices.js
@@ -18,3 +18,36 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
+
+
+const body = document.getElementById('bodyTag');
+const button = document.querySelector('#darkbutton');
+const nightBtn = document.querySelector('.night')
+const sunBtn = document.querySelector('.sun')
+
+function toggleDark() {
+  if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    localStorage.setItem("theme", "light");
+    nightBtn.style.display='block'
+    sunBtn.style.display='none'
+    
+    
+  } else {
+    body.classList.add('dark');
+    localStorage.setItem("theme", "dark");
+    sunBtn.style.display='block'
+    nightBtn.style.display='none'
+    
+  }
+}
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add('dark');
+  nightBtn.style.display='none';
+  sunBtn.style.display='block'
+ 
+  
+}
+
+document.querySelector('#darkbutton').addEventListener('click', toggleDark);
