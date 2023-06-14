1a769a783f25649837ea396ab3d6f37739859687
srvinith <r36041595@gmail.com>
2023-06-14 14:55:13 +0530

    dg

--- static/js/data.js
+++ static/js/data.js
@@ -1,45 +1,72 @@
-const data = [
-    {
-        id: 1,
-        memberImg: 'images/ram-designer.png',
-        memberName: "RamKumar",
-        Designation:"Graphic Design"
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("team-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
 
-    },
-    {
-        id: 2,
-        memberImg: 'images/ram-designer.png',
-        memberName: "Ganesh",
-        Designation:"Ui & Ux Design"
+const allBtn = document.querySelector('.allBtn')
+const managementBtn = document.querySelector('.managementBtn')
+const rndBtn = document.querySelector('.rndBtn')
+const itBtn = document.querySelector('.itBtn')
+const prBtn = document.querySelector('.prBtn')
+const civilBtn = document.querySelector('.civilBtn')
+const all = document.getElementById('all')
+const management = document.getElementById('management')
+const rnd = document.getElementById('rnd')
+const it = document.getElementById('it')
+const pr = document.getElementById('pr')
+const civil = document.getElementById('civil')
 
-    },
-    {
-        id: 3,
-        memberImg: 'images/ram-designer.png',
-        memberName: "Vinith",
-        Designation:"Web Development"
-
-    },
-];
-export default (data)
-const teamMember = document.querySelector('team-member')
-// const teamMemberName = document.querySelector('team-member-name')
-
-const show = teamMember.map((val,index) => {
-
-return(
-    `
-    <div class="empl">
-                <img src="{% static 'images/ram-designer.png' %}" alt="" class="img-fluid">
-                <h6 class="team-member-name">${val.memberName}</h6>
-                <p class="team-member-designation">${val.Designation}</p>
-               </div>
-    
-    `
-)
-   
-
+allBtn.addEventListener('click', () => {
+    all.style.display = 'block'
+    management.style.display = 'block'
+    rnd.style.display = 'block'
+    it.style.display = 'block'
+    pr.style.display = 'block'
+    civil.style.display = 'block'
 })
 
-teamMember.innerHTML = show
-
+managementBtn.addEventListener('click', () => {
+    all.style.display = 'none'
+    management.style.display = 'block'
+    rnd.style.display = 'none'
+    it.style.display = 'none'
+    pr.style.display = 'none'
+    civil.style.display = 'none'
+})
+rndBtn.addEventListener('click', () => {
+    all.style.display = 'none'
+    management.style.display = 'none'
+    rnd.style.display = 'block'
+    it.style.display = 'none'
+    pr.style.display = 'none'
+    civil.style.display = 'none'
+})
+itBtn.addEventListener('click', () => {
+    all.style.display = 'none'
+    management.style.display = 'none'
+    rnd.style.display = 'none'
+    it.style.display = 'block'
+    pr.style.display = 'none'
+    civil.style.display = 'none'
+})
+prBtn.addEventListener('click', () => {
+    all.style.display = 'none'
+    management.style.display = 'none'
+    rnd.style.display = 'none'
+    it.style.display = 'none'
+    pr.style.display = 'block'
+    civil.style.display = 'none'
+})
+civilBtn.addEventListener('click', () => {
+    all.style.display = 'none'
+    management.style.display = 'none'
+    rnd.style.display = 'none'
+    it.style.display = 'none'
+    pr.style.display = 'none'
+    civil.style.display = 'block'
+})
