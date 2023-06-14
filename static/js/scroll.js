b7ce8cc03cf708f5aee675a54d48ef4b7fd8dd25
srvinith <r36041595@gmail.com>
2023-06-14 12:43:34 +0530

    Df

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -3,6 +3,7 @@ document.addEventListener('scroll', horizontalScroll);
 //Selecting Elements
 let sticky = document.querySelector('.stickayas');
 let stickyParent = document.querySelector('.sticky-parent');
+let scrollImg = document.querySelector('.mob-s')
 
 let scrollWidth = sticky.scrollWidth;
 let verticalScrollHeight = stickyParent.getBoundingClientRect().height-sticky.getBoundingClientRect().height;
@@ -13,10 +14,13 @@ function horizontalScroll(){
     //Checking whether the sticky element has entered into view or not
     let stickyPosition = sticky.getBoundingClientRect().top;
     if(stickyPosition > 1){
+        
         return;
-    }else{
+    }
+    else{
         let scrolled = stickyParent.getBoundingClientRect().top; //how much is scrolled?
         sticky.scrollLeft =(scrollWidth/verticalScrollHeight)*(-scrolled)*0.85;
+        scrollImg.style.transform ='scale(1.1)'
     
     }
 }
