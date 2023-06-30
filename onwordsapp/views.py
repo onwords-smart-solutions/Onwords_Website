6680cfa42c8954cd98cfd1e6a022a770d4f4b241
koushikromel <koushikromel@gmail.com>
2023-06-30 12:11:13 +0530

    testing added #

--- onwordsapp/views.py
+++ onwordsapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
+## Create your views here.
 def home(request):
     return render(request,'home.html')
 def ourservices(request):
