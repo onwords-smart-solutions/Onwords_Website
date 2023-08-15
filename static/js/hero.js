7e781f83fd39297ca18a508906ffdc2eea319365
srvinith <r36041595@gmail.com>
2023-08-15 22:40:57 +0530

    dt

--- static/js/hero.js
+++ static/js/hero.js
@@ -10,26 +10,50 @@ const parkingOffBtn = document.getElementById('parkingOffBtn')
 const bolconys = document.getElementById('bolconys')
 const parkings = document.getElementById('parkings')
 
-function allLightson() {
-    if (toggle.checked == true) {
-        allLightOn.style.display = 'block'
-        allLightOff.style.display = 'none'
-        bolconyOnBtn.style.display = 'block'
-        bolconyOffBtn.style.display = 'none'
-        parkingOffBtn.style.display = 'none'
-        parkingOnBtn.style.display = 'block'
-    }
-    if (toggle.checked == false) {
-        allLightOn.style.display = 'none'
-        allLightOff.style.display = 'block'
-        bolconyOnBtn.style.display = 'none'
-        bolconyOffBtn.style.display = 'block'
-        parkingOffBtn.style.display = 'block'
-        parkingOnBtn.style.display = 'none'
-    }
-}
+// function allLightson() {
+//     if (toggle.checked == true) {
+//         allLightOn.style.display = 'block'
+//         allLightOff.style.display = 'none'
+//         bolconyOnBtn.style.display = 'block'
+//         bolconyOffBtn.style.display = 'none'
+//         parkingOffBtn.style.display = 'none'
+//         parkingOnBtn.style.display = 'block'
+//     }
+//     if (toggle.checked == false) {
+//         allLightOn.style.display = 'none'
+//         allLightOff.style.display = 'block'
+//         bolconyOnBtn.style.display = 'none'
+//         bolconyOffBtn.style.display = 'block'
+//         parkingOffBtn.style.display = 'block'
+//         parkingOnBtn.style.display = 'none'
+//     }
+// }
 
 function both() {
+        if (toggle.checked == true)  {
+            bolconyOnBtn.style.display = 'block'
+            parkingOnBtn.style.display = 'block'
+            bolconyLights.style.display = 'none'
+            parkingLights.style.display = 'none'
+            allLightOn.style.display = 'block'
+            
+        }
+        if (toggle.checked == false)  {
+            bolconyOnBtn.style.display = 'none'
+            parkingOnBtn.style.display = 'none'
+            bolconyLights.style.display = 'none'
+            parkingLights.style.display = 'none'
+            allLightOn.style.display = 'none'
+    
+        }
+        if ((bolconys.checked == false) && (parkings.checked == true)) {
+            bolconyOnBtn.style.display = 'none'
+            parkingOnBtn.style.display = 'block'
+            bolconyLights.style.display = 'none'
+            parkingLights.style.display = 'block'
+            allLightOn.style.display = 'none'
+        }
+    
     if ((bolconys.checked == true) && (parkings.checked == true)) {
         bolconyOnBtn.style.display = 'block'
         parkingOnBtn.style.display = 'block'
@@ -37,6 +61,7 @@ function both() {
         parkingLights.style.display = 'none'
         allLightOn.style.display = 'block'
     }
+    
     if ((bolconys.checked == true) && (parkings.checked == false)) {
         bolconyOnBtn.style.display = 'block'
         parkingOnBtn.style.display = 'none'
@@ -51,14 +76,14 @@ function both() {
         parkingLights.style.display = 'block'
         allLightOn.style.display = 'none'
     }
-    if ((bolconys.checked == false) && (parkings.checked == false)) {
-        bolconyOnBtn.style.display = 'none'
-        parkingOnBtn.style.display = 'none'
-        bolconyLights.style.display = 'none'
-        parkingLights.style.display = 'none'
-        allLightOn.style.display = 'none'
+    // if ((bolconys.checked == false) && (parkings.checked == false)) {
+    //     bolconyOnBtn.style.display = 'none'
+    //     parkingOnBtn.style.display = 'none'
+    //     bolconyLights.style.display = 'none'
+    //     parkingLights.style.display = 'none'
+    //     allLightOn.style.display = 'none'
 
-    }
+    // }
 }
 
 const times = document.querySelector('.times')
