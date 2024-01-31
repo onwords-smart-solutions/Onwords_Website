522f468dca9fa3911aadee6bd2e506fca7fba438
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2024-01-31 12:48:46 +0530

    SHA Keys removed

--- static/js/quotation.js
+++ static/js/quotation.js
@@ -1,41 +1,27 @@
 function back() {
-  // Use the history object to go back
   window.history.back();
 }
-
 function smarthome() {
   window.location.href = "/smart_home";
 }
-
-
 function gateautomation(){
   window.location.href = "/gate_automation";
 }
-
-
-  function security_system(){
-    window.location.href = "/security_system";
-  }
-  
-
+function security_system(){
+  window.location.href = "/security_system";
+}
 function Techease_essential(){
   window.location.href = "/techease_essential";
 }
-
-
 function Pinnacle_ecosystem(){
   window.location.href = "/pinnacle_ecosystem";
 }
-
-
 function Livingtech_premium(){
   window.location.href = "/livingtech_premium";
 }
-
 function Intellihome_ultimate(){
   window.location.href = "/intellihome_ultimate";
 }
-
 function Home_hormony(){
    window.location.href = "/home_hormony";
 }
